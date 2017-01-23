866d446092e8a2636f2daaf143bbb669ccc4e778
Pascal <wermail2002-kap@yahoo.de>
2017-01-23 23:53:04 +0100

    no space required after the first id, if it's the only one

--- commands/quote.js
+++ commands/quote.js
@@ -6,6 +6,11 @@ require('moment-duration-format');
 exports.run = async (client, msg, params = []) => {
   try {
     let response = '';
+    if (params[0].indexOf('|') !== -1) {
+      response = params[0].substr(params[0].indexOf('|') + 1);
+      params[0] = params[0].substr(0, params[0].indexOf('|'));
+    }
+    client.log(params[0]);
     const messages = await msg.channel.fetchMessages({ around: params[0], limit: 1 });
     const embed = new client.methods.Embed();
     embed.setAuthor(messages.first().member.displayName, messages.first().author.displayAvatarURL)
@@ -17,10 +22,10 @@ exports.run = async (client, msg, params = []) => {
       let breakvar = false;
       for (const i in params) {
         if (params[i].includes('|') && params[i].indexOf('|') === 0) {
-          response = params.slice(i);
+          response = params.slice(i).join(' ').substr(1);
           break;
         } else if (params[i].includes('|')) {
-          response = params[i].substr(params[i].indexOf('|')) + params.slice(i + 1);
+          response = params[i].substr(params[i].indexOf('|') + 1) + params.slice(i + 1);
           params[i] = params[i].substr(0, params[i].indexOf('|'));
           breakvar = true;
         }
@@ -31,7 +36,7 @@ exports.run = async (client, msg, params = []) => {
     }
     await msg.edit(response, { embed: embed });
   } catch (e) {
-    client.log(require('util').inspect(e, false, 3));
+    client.log(require('util').inspect(e, false, 0));
     msg.edit(`${msg.content}
 
 \`E-ROHR\`
