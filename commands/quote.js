d844c3f20f8d65f51d09af44771a57f3fa818c15
Pascal <wermail2002-kap@yahoo.de>
2017-01-23 13:44:12 +0100

    changed linebreak to "|"

--- commands/quote.js
+++ commands/quote.js
@@ -16,12 +16,12 @@ exports.run = async (client, msg, params = []) => {
       params = params.slice(1);
       let breakvar = false;
       for (const i in params) {
-        if (params[i].includes('\n') && params[i].indexOf('\n') === 0) {
+        if (params[i].includes('|') && params[i].indexOf('|') === 0) {
           response = params.slice(i);
           break;
-        } else if (params[i].includes('\n')) {
-          response = params[i].substr(params[i].indexOf('\n')) + params.slice(i + 1);
-          params[i] = params[i].substr(0, params[i].indexOf('\n'));
+        } else if (params[i].includes('|')) {
+          response = params[i].substr(params[i].indexOf('|')) + params.slice(i + 1);
+          params[i] = params[i].substr(0, params[i].indexOf('|'));
           breakvar = true;
         }
         const add = await msg.channel.fetchMessages({ around: params[i], limit: 1 });
