4a71978f81c12ba0b253e4b59a7c6aa08e327c9b
Pascal <wermail2002-kap@yahoo.de>
2017-01-25 12:55:08 +0100

    added alive command

---
+++ commands/alive.js
@@ -0,0 +1,17 @@
+exports.run = async (client, msg, params = []) => {
+  msg.delete();
+};
+
+
+exports.conf = {
+  enabled: true,
+  aliases: [],
+};
+
+
+exports.help = {
+  name: 'alive',
+  shortdescription: '-',
+  description: '-',
+  usage: '-',
+};
