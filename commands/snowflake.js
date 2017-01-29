c72751054d7a757f3ab78b58175c001d1547ffa3
Pascal <wermail2002-kap@yahoo.de>
2017-01-29 17:17:28 +0100

    Added most useful command ever

---
+++ commands/snowflake.js
@@ -0,0 +1,29 @@
+const SnowflakeUtil = require('../node_modules/discord.js/src/util/Snowflake.js');
+const moment = require('moment');
+moment.locale('de');
+require('moment-duration-format');
+
+exports.run = async (client, msg, params = []) => {
+  if (!params[0]) {
+    return msg.edit(`${msg.content} 👀`);
+  }
+  const snowflake = SnowflakeUtil.deconstruct(params[0]);
+  const date = moment(snowflake.date).format('DD.MM.YYYY HH:mm:ss');
+  return msg.edit(`${msg.content}
+  \`\`\`LDIF
+Date: ${date}\`\`\``);
+};
+
+
+exports.conf = {
+  enabled: true,
+  aliases: ['snow'],
+};
+
+
+exports.help = {
+  name: 'snowflake',
+  shortdescription: '-',
+  description: '-',
+  usage: '-',
+};
