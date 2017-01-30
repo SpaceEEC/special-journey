dcad01721917e462cfd667b2656284b0193edf0f
Pascal <wermail2002-kap@yahoo.de>
2017-01-30 14:20:40 +0100

    added time zone for snowlake command

--- commands/snowflake.js
+++ commands/snowflake.js
@@ -6,7 +6,7 @@ require('moment-duration-format');
 exports.run = async (client, msg, params = []) => {
   if (!params[0]) return msg.edit(`${msg.content} 👀`);
   const snowflake = SnowflakeUtil.deconstruct(params[0]);
-  const date = moment(snowflake.date).format('DD.MM.YYYY HH:mm:ss');
+  const date = moment(snowflake.date).format('DD.MM.YYYY HH:mm:ss [(CET)]');
   return msg.edit(`${msg.content}
   \`\`\`LDIF
 Date: ${date}\`\`\``);
