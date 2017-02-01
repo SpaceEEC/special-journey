78cfc90f9853a83a1018a8316fb7eda6d3e8761b
Pascal <wermail2002-kap@yahoo.de>
2017-02-01 01:25:50 +0100

    added translate command

---
+++ commands/translate.js
@@ -0,0 +1,60 @@
+// Much thanks to Kurisu (http://kurisubrooks.com/) for the API!
+const request = require('superagent');
+
+exports.run = async (client, msg, params = []) => {
+  const obj = {};
+  if (!params[1]) return msg.edit(`${msg.content}\n\nUnvollständig.`);
+  if (params[0].match(/^-..$/g)) {
+    obj.to = params[0].replace('-', '');
+    if (!langs.includes(obj.to)) return msg.edit(`${msg.content}\n\nUnbekannte Sprache \`${obj.to}\`!`);
+  }
+  if (params[1].match(/^-..$/g)) {
+    obj.from = params[1].replace('-', '');
+    if (!langs.includes(obj.from)) return msg.edit(`${msg.content}\n\nUnbekannte Sprache \`${obj.from}\`!`);
+    if (!params[2]) return msg.edit(`${msg.content}\n\nUnvollständig.`);
+    obj.query = params.slice(2).join(' ');
+  } else {
+    obj.query = params.slice(1).join(' ');
+  }
+  const res = await request.post(`https://api.kurisubrooks.com/api/translate`)
+    .send(obj)
+    .set('Content-Type', 'application/json');
+  if (res.body.ok) {
+    return msg.edit('', {
+      embed:
+      new client.methods.Embed()
+        .setColor(0xb89bf8)
+        .setAuthor(`API-Schnittstelle bereitgestellt von Kurisu`, 'http://kurisubrooks.com/favicon.ico', 'http://kurisubrooks.com/')
+        .addField(`Von ${res.body.from.name} (${res.body.from.local})`, res.body.query)
+        .addField(`In ${res.body.to.name} (${res.body.to.local})`, res.body.result)
+    });
+  } else {
+    return msg.edit(`${msg.content}
+\`\`\`LDIF
+${res.body.error}
+\`\`\``);
+  }
+};
+
+// Are all of them even supported by Kurisu? (I could swear there were less of them in the source of the api.)
+const langs = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bn', 'bs', 'bg', 'ca',
+  'ceb', 'ny', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi',
+  'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is',
+  'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv',
+  'lt', 'lb', 'mk', 'mg', 'ms', 'ms', 'ml', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ps', 'fa',
+  'pl', 'pt', 'ma', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es',
+  'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+
+
+exports.conf = {
+  enabled: true,
+  aliases: ['t'],
+};
+
+
+exports.help = {
+  name: 'translate',
+  shortdescription: '-',
+  description: '-',
+  usage: '-',
+};
