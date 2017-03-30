627d413950e9e688af5d426ee53513c0e303df05
SpaceEEC <wermail2002-kap@yahoo.de>
2017-03-30 20:57:44 +0200

    made token resolver better

--- commands/token.js
+++ commands/token.js
@@ -1,8 +1,17 @@
-exports.run = async (client, msg, params = []) => { // eslint-disable-line
-	msg.cmd = 'rasync';
-	params = ['const', 'u', '=', 'await', 'this.client.fetchUser(', `'${Buffer.from(params[0].split('.')[0], 'base64')}'`,
-		');', 'return', '`${u.username}#${u.discriminator}', '(${u.id})`']; // eslint-disable-line
-	client.commands.get('reval').run(client, msg, params);
+const { get } = require('superagent');
+exports.run = async (client, msg, params = []) => {
+	try {
+		const id = Buffer.from(params[0].split('.')[0], 'base64').toString();
+		if (id.search(/\D/g) !== -1) throw String('Not even remotely a snowflake.');
+		const { body: user } = await get(`https://discordapp.com/api/users/${id}`)
+			.set('Authorization', `Bot ${client.conf.botToken}`);
+		msg.edit(`${msg.content}\n\`\`\`js\n${user.username}#${user.discriminator} (${user.id})\`\`\``);
+	} catch (error) {
+		msg.edit(
+`${msg.content}\`\`\`js
+${error}${error.response && error.response.res && error.response.res.text ? `
+${error.response.res.text}` : ''}\`\`\``);
+	}
 };
 
 
