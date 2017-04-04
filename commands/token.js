e200b67140411d72979609f50b29c88a486f4198
SpaceEEC <wermail2002-kap@yahoo.de>
2017-04-04 15:56:53 +0200

    added full token support to token command and added aliases

--- commands/token.js
+++ commands/token.js
@@ -1,11 +1,20 @@
 const { get } = require('snekfetch');
 
 exports.run = async (client, msg, params = []) => {
+	let url;
+	let auth;
 	try {
-		const id = Buffer.from(params[0].split('.')[0], 'base64').toString();
-		if (id.search(/\D/g) !== -1) throw String('Not even remotely a snowflake.');
-		const { body: user } = await get(`https://discordapp.com/api/users/${id}`)
-			.set('Authorization', `Bot ${client.conf.botToken}`);
+		if (msg.cmd === 'token') {
+			url = 'https://discordapp.com/api/v6/users/@me';
+			auth = params.join(' ');
+		} else {
+			const id = Buffer.from(params[0].split('.')[0], 'base64').toString();
+			if (id.search(/\D/g) !== -1) throw String('Not even remotely a snowflake.');
+			url = `https://discordapp.com/api/v6/users/${id}`;
+			auth = `Bot ${client.conf.botToken}`;
+		}
+		const { body: user } = await get(url)
+			.set('Authorization', auth);
 		msg.edit(`${msg.content}\n\`\`\`js\n${user.username}#${user.discriminator} (${user.id})\`\`\``);
 	} catch (error) {
 		console.log(error);
@@ -19,7 +28,7 @@ ${error.url ? `${error.status} ${error.statusText}\n${error.text}` : error}\`\`\
 
 exports.conf = {
 	enabled: true,
-	aliases: []
+	aliases: ['id', 'base64']
 };
 
 
