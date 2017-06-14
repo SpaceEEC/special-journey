fb97f494fc1a56d3a877a389dc79b9ed38766e5d
SpaceEEC <wermail2002-kap@yahoo.de>
2017-06-14 13:24:05 +0200

    type notations for the EventCounter

--- src/structures/EventCounter.ts
+++ src/structures/EventCounter.ts
@@ -1,17 +1,42 @@
 // copy pasted from gus' EventCount and added type notations
 import { stripIndents } from 'common-tags';
 
+/**
+ * EventCounter class for the statisticsTM.
+ */
 export class EventCounter {
-	public events: { [type: string]: number };
-	public ops: { [type: number]: number };
-	public frequency: number;
-	public total: number;
-	public start: Date;
+	/**
+	 * Stored event names with a count
+	 */
+	private events: { [type: string]: number };
+	/**
+	 * Stored OP codes with a count
+	 */
+	private ops: { [type: number]: number };
+	/**
+	 * Frequency of events per second
+	 */
+	private frequency: number;
+	/**
+	 * Total number of events
+	 */
+	private total: number;
+	/**
+	 * Starting point
+	 */
+	private start: Date;
 
+	/**
+	 * Instantiates a new EventCounter
+	 */
 	public constructor() {
 		this.reset();
 	}
 
+	/**
+	 * Resets the EventCounter's stats
+	 * @returns {void}
+	 */
 	public reset(): void {
 		this.events = {};
 		this.ops = {};
@@ -20,6 +45,11 @@ export class EventCounter {
 		this.start = null;
 	}
 
+	/**
+	 * To be called on every raw event.
+	 * @param {string|number} event The event name or OP code
+	 * @return {void}
+	 */
 	public trigger(event: string | number): void {
 		if (typeof event === 'string') {
 			if (!this.events[event]) this.events[event] = 0;
@@ -33,9 +63,10 @@ export class EventCounter {
 		this.frequency = ++this.total / ((Date.now() - +this.start) / 1000);
 	}
 
-	public get seconds(): number {
-		return (Date.now() - +this.start) / 1000;
-	}
+	/**
+	 * Custom inspector for a nicer display.
+	 * @returns {string}
+	 */
 	public inspect(): string {
 		if (Object.keys(this.events).length <= 1) return `EventCounter ${this.events}`;
 		return stripIndents`
