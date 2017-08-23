f00abc12f71b376eacaeb0fd1c593710e92003b1
SpaceEEC <wermail2002-kap@yahoo.de>
2017-08-23 10:12:50 +0200

    Preparing for rewrite

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { SelfbotClient } from './structures/client';
-import { Logger } from './structures/logger';
-
-const client: SelfbotClient = new SelfbotClient();
-client.login(client.config.token);
-
-process.on('unhandledRejection', (err: any) =>
-	Logger.instance.error('unhandledRejection:', err),
-);
