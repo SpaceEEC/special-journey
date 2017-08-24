4fff6881a9a77aec6751a038489c28d2176035d0
SpaceEEC <wermail2002-kap@yahoo.de>
2017-08-24 13:34:26 +0200

    Fixed sourcemaps and updated/changed deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,45 @@
 const gulp = require('gulp');
 const gulpTs = require('gulp-typescript');
-const gulpTslint = require('gulp-tslint');
 const tslint = require('tslint');
+const gulpTslint = require('gulp-tslint');
+const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
+const { join } = require('path');
 
 const project = gulpTs.createProject('tsconfig.json');
 const typeCheck = tslint.Linter.createProgram('tsconfig.json');
 
-gulp.task('lint', () => {
+gulp.task('lint', () =>
+{
 	gulp.src('./src/**/*.ts')
-		.pipe(gulpTslint({
-			configuration: 'tslint.json',
-			formatter: 'prose',
-			program: typeCheck
-		}))
+		.pipe(gulpTslint(
+			{
+				configuration: 'tslint.json',
+				formatter: 'prose',
+				program: typeCheck
+			},
+		))
 		.pipe(gulpTslint.report());
 })
 
 gulp.task('build', () => {
 	del.sync(['./bin/**/*.*']);
-	gulp.src('./src/**/*.ts')
-		.pipe(project())
-		.pipe(gulp.dest('bin/'));
-	gulp.src('./src/**/*.js')
-		.pipe(gulp.dest('bin/'));
-	gulp.src('./src/**/*.json')
-		.pipe(gulp.dest('bin/'));
-	gulp.src('./src/**/*.png')
-		.pipe(gulp.dest('bin/'));
-	gulp.src('./src/**/*.ttf')
+
+	const tsCompile = gulp.src('./src/**/*.ts')
+		.pipe(sourcemaps.init({ base: 'src' }))
+		.pipe(project());
+
+	tsCompile.pipe(gulp.dest('bin/'));
+
+	tsCompile.js
+		.pipe(sourcemaps.mapSources(sourcePath => join(__dirname, 'src', sourcePath)))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('bin/'));
 });
+
+gulp.task('watch', () =>
+{
+	gulp.watch('./src/**/*.ts', ['lint', 'build']);
+});
+
+gulp.task('default', ['watch']);
