a6c59943f2b36cb98dc315dacdf9f63be5f1204b
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-08 23:53:55 +0200

    refactor(CodeBlocks): simplify responses and remove unnecessary variables

--- src/Commands/CodeBlock.ts
+++ src/Commands/CodeBlock.ts
@@ -1,7 +1,8 @@
 import { Message } from 'discord.js';
 
-import { Command } from '../Structures/Command';
+import { Aliases, Command } from '../Structures/Command';
 
+@Aliases('CB')
 export class CodeBlockCommand extends Command
 {
 	private _codeBlock: RegExp = /```(.+)\n/g;
@@ -12,20 +13,18 @@ export class CodeBlockCommand extends Command
 
 		const fetched: Message = await msg.channel.messages.fetch(id).catch(() => null);
 
-		if (!fetched) return msg.edit('No message with that id found.');
+		if (!fetched) return msg.edit('Could not find the provided message.');
 
-		let result: string = 'Codeblocks:\n';
-		let found: boolean = false;
+		const found: string[] = [];
 		let match: string[];
 		// tslint:disable-next-line:no-conditional-assignment
 		while ((match = this._codeBlock.exec(fetched.content)))
 		{
-			if (!found) found = true;
-			result += `\`${match[1]}\`\n`;
+			found.push(match[1]);
 		}
 
-		if (!found) result = 'No codeblocks found in the provided message';
+		if (!found.length) return msg.edit('Could not find any codeblocks in the provided message.');
 
-		return msg.edit(result);
+		return msg.edit(found.join('\n'));
 	}
 }
