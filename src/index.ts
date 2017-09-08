1be6c3ca0ba06d01e329032fc0fdcf4f1e0d15c9
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-08 23:47:58 +0200

    style(index): remove wrong space between tags when catching an error on login

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ const { TOKEN }: { [key: string]: string } = process.env;
 process.on('unhandledRejection', (error: Error) => Logger.instance.error('[Unhandled Rejection]', error));
 
 const client: Client = new Client({ disableEveryone: true });
-client.login(TOKEN).catch((error: Error) => Logger.instance.error('[DJS] [LOGIN]', error));
+client.login(TOKEN).catch((error: Error) => Logger.instance.error('[DJS][LOGIN]', error));
